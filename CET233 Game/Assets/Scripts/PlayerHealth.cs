08c28f2497177657e1d95a86dcf82401a068702e
Smthy <61051295+Smthy@users.noreply.github.com>
2020-04-16 14:40:40 +0100

    Added Health System
    Slightly Laggy (Probably my own PC)

---
+++ CET233 Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float pHealth = 100f;
+    public GameObject player, pCamera;
+
+    void Start()
+    { 
+
+    }
+
+    void Update()
+    {
+        if (pHealth >= 100)
+        {
+            pHealth = 100f;
+        }
+
+        if (pHealth <= 0)
+        {
+            pHealth = 0;
+        }
+    }
+
+    public void PlayerTakeDamage(float amount)
+    {
+        pHealth -= amount;
+
+        if (pHealth <= 0f)
+        {
+            Destroy(this);
+        }
+    }    
+}
