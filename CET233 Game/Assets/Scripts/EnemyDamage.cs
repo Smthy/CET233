86971209745d72d53a68554034526e305c78a84e
Smthy <61051295+Smthy@users.noreply.github.com>
2020-04-23 14:01:54 +0100

    Health Added

---
+++ CET233 Game/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public GameObject player;
+    public float impactDamage = 75f;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerHealth playerH = player.GetComponent<PlayerHealth>();
+            if (playerH != null)
+            {
+                playerH.PlayerTakeDamage(impactDamage);
+            }
+        }
+    }
+}
