e1506dcb45e9428fd65ab73313f6709f7ad1f122
Smthy <61051295+Smthy@users.noreply.github.com>
2020-05-07 10:59:02 +0100

    added shrooms

---
+++ CET233 Game/Assets/Scripts/MushroomIncrease.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomIncrease : MonoBehaviour
+{
+    public ParticleSystem mushroom;
+
+    void Start()
+    {
+        mushroom = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystemEmissionType.Time = 10;
+    }
+}
